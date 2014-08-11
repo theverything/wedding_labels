488df245222228b9f047659b57484933cba9eef1
theverything <j3ffhorn@gmail.com>
2014-08-10 23:35:30 -0700

    refactor labels

--- index.rb
+++ index.rb
@@ -1,18 +1,33 @@
 require_relative './lib/labels'
 require_relative './lib/counter'
 
-Dir.glob('./csv/people/*.csv').each do |file|
-  opts = {
-    cols: 3,
-    rows: 10,
-    left_margin: 13.25,
-    right_margin: 13.25,
-    top_margin: 35.25,
-    bottom_margin: 35.25,
-    path: file,
-    dest: './labels',
-    font: "#{Dir.pwd}/fonts/brandonlight.ttf"
-  }
-  labels = Labels.new(opts, Counter)
-  labels.create
-end
+# # guests
+# Dir.glob('./csv/guests/*.csv').each do |file|
+#   opts = {
+#     cols: 3,
+#     rows: 10,
+#     left_margin: 13.25,
+#     right_margin: 13.25,
+#     top_margin: 35.25,
+#     bottom_margin: 35.25,
+#     path: file,
+#     dest: './labels',
+#     font: "#{Dir.pwd}/fonts/brandonmed.ttf"
+#   }
+#   labels = Labels.new(opts, Counter)
+#   labels.create
+# end
+# single
+opts = {
+  cols: 3,
+  rows: 10,
+  left_margin: 13.25,
+  right_margin: 13.25,
+  top_margin: 35.25,
+  bottom_margin: 35.25,
+  dest: './labels',
+  font: "#{Dir.pwd}/fonts/brandonmed.ttf",
+  single: ["Kat Park & Jeff Horn", "311 Centinary Dr", "Walnut, CA 91789"]
+}
+labels = Labels.new(opts, Counter)
+labels.create
--- lib/labels.rb
+++ lib/labels.rb
@@ -11,35 +11,51 @@ class Labels
       bottom_margin: opts[:bottom_margin]
     }
     @path = opts[:path]
-    @name = File.basename(@path, '.csv')
+    @name =  File.basename(@path, '.csv')
       .gsub(' ', '')
       .gsub('-', '_')
-      .downcase
-    @counter = counter
+      .downcase if @path
     @dest = opts[:dest]
     @cols = opts[:cols]
     @rows = opts[:rows]
     @font = opts[:font]
+    @single = opts[:single] || false
+    @counter = counter.new(@cols, @rows)
+    @pdf = create_doc()
   end
 
-  def create
-    cols = @cols
-    rows = @rows
-    font_path = @font
-    labels = CSV.read(@path)
-    counter = @counter.new(cols, rows)
 
-    Prawn::Document.generate("#{@dest}/label_#{@name}.pdf", @margins) do
-      define_grid(columns: cols, rows: rows, row_gutter: 0, column_gutter: 10)
-      font font_path
-      font_size 10
-      labels.each do |label|
-        counts = counter.count
-        grid(counts[:row], counts[:col]).bounding_box do
-          bounding_box([20, 60], :width => 150) do
-            text label.join("\n")
-          end
-        end
+  def create
+    if @single
+      @name = "single"
+      label = @single
+      30.times do
+        counts = @counter.count
+        create_label(label, counts)
+      end
+    else
+      labels = CSV.read(@path)
+      labels.each do |l|
+        counts = @counter.count
+        create_label(l, counts)
+      end
+    end
+    @pdf.render_file("#{@dest}/label_#{@name}.pdf")
+  end
+
+  private
+  def create_doc
+    pdf = Prawn::Document.new
+    pdf.define_grid(columns: @cols, rows: @rows, row_gutter: 0, column_gutter: 10)
+    pdf.font @font
+    pdf.font_size 10
+    pdf
+  end
+
+  def create_label(data, counts)
+    @pdf.grid(counts[:row], counts[:col]).bounding_box do
+      @pdf.bounding_box([20, 60], :width => 150) do
+        @pdf.text data.join("\n").upcase
       end
     end
   end
